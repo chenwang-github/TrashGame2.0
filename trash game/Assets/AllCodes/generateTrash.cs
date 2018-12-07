fc12f4ee396d20df35bfaf1fcb2dfbb698009ed6
chenwang <cwang19@uci.edu>
2018-12-06 22:04:37 -0800

    from jake

--- trash game/Assets/AllCodes/generateTrash.cs
+++ trash game/Assets/AllCodes/generateTrash.cs
@@ -4,12 +4,7 @@ using UnityEngine;
 
 public class generateTrash : MonoBehaviour {
 
-
-	public List<GameObject> recycleSpawn = new List<GameObject>();
-	public List<GameObject> compsiteSpawn = new List<GameObject>();
-	public List<GameObject> landfillSpawn = new List<GameObject>();
-
-
+	public List<GameObject> itemsToSpawn = new List<GameObject>();
 	//public float generateRate = 7F;
 	bool dropping;
 
@@ -29,23 +24,12 @@ public class generateTrash : MonoBehaviour {
 
 		while (true)
 		{
-			int typeidx = Random.Range(0, 3);
-			int itemidx = Random.Range(0, 5);
-			GameObject toBeSpawn;
-
- 			if (typeidx == 0){
-				toBeSpawn = recycleSpawn[itemidx];
-			}else if(typeidx == 1){
-				toBeSpawn = compsiteSpawn[itemidx];
-			}else{
-				toBeSpawn = landfillSpawn[itemidx];
-			}
-
+			int num = Random.Range(0, itemsToSpawn.Count);
 			float sizeZ = GetComponent<Collider>().bounds.size.z / 2;
 
 			Vector3 pos = new Vector3(transform.position.x, transform.position.y, Random.Range(-sizeZ, sizeZ));
 
-			GameObject item = Instantiate(toBeSpawn, pos, Quaternion.identity);
+			GameObject item = Instantiate(itemsToSpawn[num], pos, Quaternion.identity);
 
 			yield return new WaitForSeconds(GameManager.instance.generateRate);
 		}
