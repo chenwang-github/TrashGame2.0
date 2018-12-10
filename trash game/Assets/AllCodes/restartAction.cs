dc30a8eb6d3968f0fca3db7984dcd3f3dc078ded
chenwang <cwang19@uci.edu>
2018-12-10 11:50:07 -0800

    GarbageGameFinal

---
+++ trash game/Assets/AllCodes/restartAction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restartAction : MonoBehaviour {
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            Debug.Log("aaaaaa");
+            Globals.score = 0;
+            Globals.meter = 5;
+            Globals.gg = false;
+            transform.position = new Vector3(64, -64, 7);
+
+        }
+    }
+
+}
